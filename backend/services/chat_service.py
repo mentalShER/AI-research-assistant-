5b28ea3c67160a30d2935a2009126779913ec7e2
mentalSHER <charuthorve12@gmail.com>
2026-07-14 17:12:17 +0530

    Ticket #5: Integrate Gemini API

--- backend/services/chat_service.py
+++ backend/services/chat_service.py
@@ -1,2 +1,17 @@
-def generate_reply(message: str) -> str:
-    return "Hello! I'm your AI Assistant."
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+def generate_reply(message: str):
+    response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=message
+    )
+
+    return response.text
